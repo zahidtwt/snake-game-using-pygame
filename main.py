6a1efe3eec08f7f7375e9527fafe1c3f66564174
Zahid Ul Islam <zahid@Zahids-MacBook-Air.local>
2022-12-06 20:59:48 +0600

    added escape key

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import pygame
-import time
+from pygame.locals import *
 if __name__ == "__main__":
     pygame.init()
     surface = pygame.display.set_mode((1000, 500))
     surface.fill((255, 200, 100))
     pygame.display.flip()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+
+        elif event.type == QUIT:
+            running = False
