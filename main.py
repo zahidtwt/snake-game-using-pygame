36b0bfaa02595954bb14fd2900626e44e8474c40
Zahid Ul Islam <zahid@Zahids-MacBook-Air.local>
2022-12-06 20:31:40 +0600

    updating

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pygame
+import time
 if __name__ == "__main__":
     pygame.init()
-    surface = pygame.display.set_mode((500, 500), )
+    surface = pygame.display.set_mode((1000, 500))
+    surface.fill((255, 200, 100))
+    pygame.display.flip()
